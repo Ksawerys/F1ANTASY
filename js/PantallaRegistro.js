a08c9232e5a4a479235305c53a332c826f38769d
Alejandro <alex.garcia.2002@hotmail.com>
2023-11-02 10:10:15 +0100

    inputsjs

---
+++ js/PantallaRegistro.js
@@ -0,0 +1,16 @@
+import {Persona} from './Clases'
+
+var inputNombre = document.getElementById('name')
+var inputApellido = document.getElementById('sname')
+var inputNick = document.getElementById('nick')
+var inputEmail = document.getElementById('email')
+var inputPassword = document.getElementById('password')
+var inputPasswordVerified = document.getElementById('passwordVerified')
+
+
+
+
+
+
+
+
