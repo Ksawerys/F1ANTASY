b1a320b110414d4bbf841bd7578531fa150daa06
Alejandro <alex.garcia.2002@hotmail.com>
2023-11-02 10:00:58 +0100

    class persona

---
+++ js/Clases.js
@@ -0,0 +1,9 @@
+export class Persona{
+    constructor(nombre,apellidos,nick,correo,password){
+        this.nombre = nombre
+        this.apellidos = apellidos,
+        this.nick = nick
+        this.correo = correo
+        this.password = password
+    }
+}
