40fdabfb6127d3b45868f1e05fd4313c128b9dfa
Alejandro <alex.garcia.2002@hotmail.com>
2023-11-06 19:23:40 +0100

    Id colocadas

---
+++ js/Perfil.js
@@ -0,0 +1,8 @@
+var usuario = JSON.parse(localStorage.getItem("user"))
+
+var nombreLabel = document.getElementById("nombreLabel")
+var apellidoLabel = document.getElementById("apellidoLabel")
+var nickLabel = document.getElementById("nickLabel")
+var emailLabel = document.getElementById("emailLabel")
+var nombreLabel = document.getElementById("nombreLabel")
+
