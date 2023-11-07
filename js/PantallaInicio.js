e6bbf0e10a1521d24f2e94d94103861c16e97fe6
Alejandro <alex.garcia.2002@hotmail.com>
2023-11-07 20:32:13 +0100

    Cambios visibles pantalla pilotos

--- js/PantallaInicio.js
+++ js/PantallaInicio.js
@@ -5,16 +5,16 @@ let descProxCarreraLabel = document.getElementById("descLabel")
 const imagen = document.getElementById("imagenCircuit");
 let proximoGranPremioNoDisputado = null
 
-const pilotosF1 = [
-    new Piloto(1, "Lewis", "Hamilton",0, "Reino Unido", 0, "ocupado"),
-    new Piloto(2, "Max", "Verstappen",0, "Países Bajos", 0, "reserva"),
-    new Piloto(3, "Valtteri", "Bottas",0, "Finlandia", 0, "ocupado"),
-    new Piloto(4, "Sergio", "Pérez",0, "México", 0, "reserva"),
+export const pilotosF1 = [
+    new Piloto(1, "Lewis", "Hamilton",0, "Reino Unido", 0, "Titular"),
+    new Piloto(2, "Max", "Verstappen",0, "Países Bajos", 0, "Suplente"),
+    new Piloto(3, "Valtteri", "Bottas",0, "Finlandia", 0, "Titular"),
+    new Piloto(4, "Sergio", "Pérez",0, "México", 0, "Suplente"),
     new Piloto(5, "Lando", "Norris",0, "Reino Unido", 0, "libre"),
     new Piloto(6, "Nico", "Hulkenberg",0, "Alemania", 0, "libre"),
-    new Piloto(7, "Carlos", "Sainz",0, "España", 0, "reserva"),
+    new Piloto(7, "Carlos", "Sainz",0, "España", 0, "Suplente"),
     new Piloto(8, "Charles", "Leclerc",0, "Mónaco", 0, "libre"),
-    new Piloto(9, "Fernando", "Alonso",0, "España", 0, "ocupado"),
+    new Piloto(9, "Fernando", "Alonso",0, "España", 0, "Titular"),
     new Piloto(10, "Esteban", "Ocon",0, "Francia", 0, "libre"),
     new Piloto(11, "Nyck", "De Vries",0, "Paises Bajos", 0, "libre"),
     new Piloto(12, "Lance", "Stroll",0, "Canadá", 0, "libre"),
