db1616f6c7de67397a60ac5a674a48ca32cc2085
Alejandro <alex.garcia.2002@hotmail.com>
2023-11-10 10:15:35 +0100

    Clasificacion listo

--- js/Clasificacion.js
+++ js/Clasificacion.js
@@ -1,30 +1,33 @@
-const personas = [
-    JSON.parse(localStorage.getItem("user")),
-    JSON.parse(localStorage.getItem("bot1")),
-    JSON.parse(localStorage.getItem("bot2"))
-  ];
+document.addEventListener('DOMContentLoaded', () => {
+    const personas = [
+        JSON.parse(localStorage.getItem("user")),
+        JSON.parse(localStorage.getItem("bot1")),
+        JSON.parse(localStorage.getItem("bot2"))
+    ];
 
-  
-  
-  personas.sort((a, b) => b.puntos - a.puntos);
-  
-  const topTresPersonas = personas.slice(0, 3);
-  
-  document.addEventListener('DOMContentLoaded', () => {
+    // Filtra los jugadores que no están definidos en el localStorage
+    const jugadoresDefinidos = personas.filter(jugador => jugador);
+
+    // Ordena los jugadores por puntos de mayor a menor
+    jugadoresDefinidos.sort((a, b) => b.puntos - a.puntos);
+
+    // Selecciona los tres primeros jugadores
+    const topTresPersonas = jugadoresDefinidos.slice(0, 3);
+
+    // Muestra la información en la página
     const jugadorElements = document.querySelectorAll('.informacion');
-  
+
     for (let i = 0; i < jugadorElements.length; i++) {
-      const jugador = topTresPersonas[i];
-      const nombreElement = jugadorElements[i].querySelector('#Nick');
-      const puntuacionElement = jugadorElements[i].querySelector('#Puntuacion');
-      const imagenElement = document.getElementById(`img${i}`)
-  
-      nombreElement.textContent = jugador.nombre;
-      puntuacionElement.textContent = `Puntuacion: ${jugador.puntos}`;
-      
-      // Cambia la ruta de la imagen
-      const imagenSrc = `../Perfiles/${jugador.nombre}.png`;
-      imagenElement.src = imagenSrc;
+        const jugador = topTresPersonas[i];
+        const nombreElement = jugadorElements[i].querySelector('#Nick');
+        const puntuacionElement = jugadorElements[i].querySelector('#Puntuacion');
+        const imagenElement = document.getElementById(`img${i}`)
+
+        nombreElement.textContent = jugador.nombre;
+        puntuacionElement.textContent = `Puntuacion: ${jugador.puntos}`;
+
+        // Cambia la ruta de la imagen
+        const imagenSrc = `../Perfiles/${jugador.nombre}.png`;
+        imagenElement.src = imagenSrc;
     }
-  });
-  
+});
